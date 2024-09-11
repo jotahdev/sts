83fc5c91d1871224d22d01d0a25a2ab7179e9797
joao <jotah@gmail.com>
2024-09-11 18:41:07 -0300

    primeiro commit

---
+++ src/dr/comjmarcos/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+package dr.comjmarcos;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args [0];
+		
+		if (ambiente.equals("DEV")) {
+			System.out.print("DEV");
+			
+		}
+
+	}
+
+}
